0f4f864b61a7a9baec5a922cbc18912708f6898e
tan.minhang <wander@3-dragons.com>
2020-08-11 16:06:28 +0800

    pin

---
+++ RT01_Template/Driver/Src/Lpspi.c
@@ -0,0 +1,10 @@
+/******************************************************************************
+Copyright (C) 2020 Querda Electronic Technology All Rights Reserved
+
+This software and code can be freely used for study and research. 
+For commercial purposes, please contact the owner for permission.
+
+File  : Port.h For Nxp S32K14x Series
+Author: Steven 
+******************************************************************************/
+#include "S32K144.h"    /* include peripheral declarations S32K144 */
--- S32k_Demo/System/EcuM/EcuM.h
+++ S32k_Demo/System/EcuM/EcuM.h
@@ -4,6 +4,10 @@ Copyright (C) 2019 Steven, mculib.net All Rights Reserved
 This software and code can be freely used for study and research. 
 For commercial purposes, please contact the owner for permission.
 
-File  : EcuM.c  Ecu Manage Unit
+File  : EcuM.h  Ecu Manage Unit
 Author: 
 ******************************************************************************/
+#ifndef ECUM_H
+#define ECUM_H
+
+#endif
